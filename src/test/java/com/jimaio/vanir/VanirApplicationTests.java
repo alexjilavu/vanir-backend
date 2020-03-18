f9774f63cb87082b8176a20869513daaa0940a83
Alex Jilavu <alexjilavu17@gmail.com>
2020-03-18 16:53:30 +0200

    Boilerplate code first commit

---
+++ src/test/java/com/jimaio/vanir/VanirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimaio.vanir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
